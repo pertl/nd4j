b912e5201137905906813fd1ef968d57c433d7d2
Peter Ertl <peter.ertl@gmx.net>
2018-01-04 00:07:34 +0100

    check terminate

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/memory/provider/TerminateHangs.java
@@ -0,0 +1,9 @@
+package org.nd4j.linalg.memory.provider;
+
+import org.nd4j.linalg.factory.Nd4j;
+
+public class TerminateHangs {
+	public static void main(final String[] args) throws Exception {
+		System.out.println(Nd4j.zeros(10));
+	}
+}
